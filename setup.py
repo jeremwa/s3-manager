65162fda4d1472065ea40c9f87ac5e1523a38ea1
Troy Larson <troylar@gmail.com>
2018-02-26 13:57:09 +0000

    Fix linter issues #5

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open('requirements.txt') as f:
 setup(
     name='s3_manager',
     version="1.0",
-    author = "Jeremy Walker",
-    author_email = "jeremw@amazon.com",
-    license = "BSD",
+    author="Jeremy Walker",
+    author_email="jeremw@amazon.com",
+    license="BSD",
     packages=find_packages(),
     include_package_data=False,
     install_requires=required,
-    entry_points = {
+    entry_points={
         "console_scripts": ['s3-manager=s3_manager.s3_manager:main']
     },
 )
